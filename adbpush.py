315271ab9af3dddb1b5c5daa6822486dc83ed7f1
Air2018 <35887548+Air2018@users.noreply.github.com>
2020-01-06 10:18:33 +0800

    修正Install:搜索

--- adbpush.py
+++ adbpush.py
@@ -14,18 +14,19 @@ def doAdbPush(fileName):
     with open(fileName, "r") as f:
         for line in f:
             line = line.strip()
-            if line.startswith("Install:"):
-                path = line[8:].strip()
+            find_pos = line.find("Install:")
+            if find_pos >= 0:
+                path = line[find_pos + 8:].strip()
                 # print(path)
                 pos = 0
                 for sub in re.finditer('/', path):
                     pos = sub.start()
                     # print(pos)
-                    if pos > 20:  # > lens("out/target/product/")
+                    if pos > 20:  # lens(out/target/product/)
                         break
                 # print("pos: " + str(pos))
                 target_path = path[pos:]
-                cmd = "adb push " + path + " " + target_path
+                cmd = "adb push " + path + " " + target_path + "\n"
                 print(cmd)
                 # time.sleep(0.5)
                 os.system(cmd)
@@ -34,6 +35,9 @@ def doAdbPush(fileName):
 
 if __name__ == '__main__':
     start = time.time()
-    os.system("adb wait-for-device root;adb wait-for-device aremount;")
+    print("Waiting for connected adb device")
+    os.system("adb devices")
+    os.system("adb wait-for-device root")
+    os.system("adb wait-for-device remount")
     doAdbPush(sys.argv[1])
     print('\033[32m Done! Cost Time: %d seconds \033[0m' % (time.time() - start))
